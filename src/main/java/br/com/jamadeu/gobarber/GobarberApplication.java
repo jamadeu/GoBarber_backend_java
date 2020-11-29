29a6a1be8ae68756f5047aff5491934ffab54f5b
Jean Amadeu <jean.amadeu2@gmail.com>
2020-11-29 09:54:19 -0300

    Start project

---
+++ src/main/java/br/com/jamadeu/gobarber/GobarberApplication.java
@@ -0,0 +1,13 @@
+package br.com.jamadeu.gobarber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GobarberApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GobarberApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/jamadeu/gobarber/GobarberApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jamadeu.gobarber;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GobarberApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
