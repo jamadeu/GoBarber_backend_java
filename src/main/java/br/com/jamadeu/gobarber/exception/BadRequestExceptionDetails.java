1bccce405fe3d7d47a1afff09f5e1f633f9677b9
Jean Amadeu <jean.amadeu2@gmail.com>
2020-11-29 10:14:22 -0300

    feat: rest exception handler

---
+++ src/main/java/br/com/jamadeu/gobarber/exception/BadRequestExceptionDetails.java
@@ -0,0 +1,9 @@
+package br.com.jamadeu.gobarber.exception;
+
+import lombok.Getter;
+import lombok.experimental.SuperBuilder;
+
+@Getter
+@SuperBuilder
+public class BadRequestExceptionDetails extends ExceptionDetails {
+}
