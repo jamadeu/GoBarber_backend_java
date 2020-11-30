da15a699dff2bc6340cb884fd72c4747a40c11c3
Jean Amadeu <jean.amadeu2@gmail.com>
2020-11-30 05:15:58 -0300

    feat: create user controller

---
+++ src/main/java/br/com/jamadeu/gobarber/controller/UserController.java
@@ -0,0 +1,13 @@
+package br.com.jamadeu.gobarber.controller;
+
+import br.com.jamadeu.gobarber.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("users")
+@RequiredArgsConstructor
+public class UserController {
+    private final UserService userService;
+}
