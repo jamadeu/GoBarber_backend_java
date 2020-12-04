2cac7d0e058b4ce69f41de2d6d211be1ae20cee6
Jean Amadeu <jean.amadeu2@gmail.com>
2020-12-04 08:44:29 -0300

    chore: add some swagger annotations

--- src/main/java/br/com/jamadeu/gobarber/controller/EmailController.java
+++ src/main/java/br/com/jamadeu/gobarber/controller/EmailController.java
@@ -2,6 +2,9 @@ package br.com.jamadeu.gobarber.controller;
 
 import br.com.jamadeu.gobarber.requests.SendForgotPasswordEmailRequest;
 import br.com.jamadeu.gobarber.service.EmailService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,6 +22,13 @@ public class EmailController {
     private final EmailService emailService;
 
     @PostMapping
+    @Operation(summary = "Send forgot password email",
+            tags = {"emails"}
+    )
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Successful operation"),
+            @ApiResponse(responseCode = "400", description = "When user not found")
+    })
     public ResponseEntity<String> sendForgotPasswordEmail(
             @RequestBody @Valid SendForgotPasswordEmailRequest sendForgotPasswordEmailRequest) {
         return new ResponseEntity<>(
