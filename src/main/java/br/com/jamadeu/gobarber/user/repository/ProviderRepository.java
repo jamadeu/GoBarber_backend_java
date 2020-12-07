6772637c4c105bc4a13ce080d1a5068e6a7be33f
Jean Amadeu <jean.amadeu2@gmail.com>
2020-12-07 10:42:11 -0300

    feat: ProviderRepository

--- src/main/java/br/com/jamadeu/gobarber/user/repository/ProviderRepository.java
+++ src/main/java/br/com/jamadeu/gobarber/user/repository/ProviderRepository.java
@@ -1,14 +1,13 @@
 package br.com.jamadeu.gobarber.user.repository;
 
 import br.com.jamadeu.gobarber.user.domain.GoBarberProvider;
-import br.com.jamadeu.gobarber.user.domain.GoBarberUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface ProviderRepository extends JpaRepository<GoBarberUser, Long> {
+public interface ProviderRepository extends JpaRepository<GoBarberProvider, Long> {
     Optional<GoBarberProvider> findByEmail(String email);
 
     Optional<GoBarberProvider> findByUsername(String username);
