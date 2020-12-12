a683a5ead8ce8f75df8af144ffd31d8b234eb319
Jean Amadeu <jean.amadeu2@gmail.com>
2020-12-12 13:28:18 -0300

    refact: refactor UserMapper

--- src/main/java/br/com/jamadeu/gobarber/modules/user/mapper/UserMapper.java
+++ src/main/java/br/com/jamadeu/gobarber/modules/user/mapper/UserMapper.java
@@ -7,17 +7,24 @@ import br.com.jamadeu.gobarber.modules.user.requests.NewUserRequest;
 import br.com.jamadeu.gobarber.modules.user.requests.ReplaceProviderRequest;
 import br.com.jamadeu.gobarber.modules.user.requests.ReplaceUserRequest;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
 @Mapper(componentModel = "spring")
 public abstract class UserMapper {
     public static final UserMapper INSTANCE = Mappers.getMapper(UserMapper.class);
 
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "authorities", ignore = true)
     public abstract GoBarberUser toUser(NewUserRequest newUserRequest);
 
+    @Mapping(target = "authorities", ignore = true)
     public abstract GoBarberUser toUser(ReplaceUserRequest replaceUserRequest);
 
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "authorities", ignore = true)
     public abstract GoBarberProvider toProvider(NewProviderRequest newProviderRequest);
 
+    @Mapping(target = "authorities", ignore = true)
     public abstract GoBarberProvider toProvider(ReplaceProviderRequest replaceProviderRequest);
 }
