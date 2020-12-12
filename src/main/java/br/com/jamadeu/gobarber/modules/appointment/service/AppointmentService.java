038d29f75b12e0978bb4002d929800e12e6d2c33
Jean Amadeu <jean.amadeu2@gmail.com>
2020-12-12 12:43:48 -0300

    refact: refactor AppointmentService

--- src/main/java/br/com/jamadeu/gobarber/modules/appointment/service/AppointmentService.java
+++ src/main/java/br/com/jamadeu/gobarber/modules/appointment/service/AppointmentService.java
@@ -3,8 +3,11 @@ package br.com.jamadeu.gobarber.modules.appointment.service;
 import br.com.jamadeu.gobarber.modules.appointment.domain.Appointment;
 import br.com.jamadeu.gobarber.modules.appointment.repository.AppointmentRepository;
 import br.com.jamadeu.gobarber.modules.appointment.requests.NewAppointmentRequest;
-import br.com.jamadeu.gobarber.shared.exception.BadRequestException;
+import br.com.jamadeu.gobarber.modules.user.domain.GoBarberProvider;
+import br.com.jamadeu.gobarber.modules.user.domain.GoBarberUser;
+import br.com.jamadeu.gobarber.modules.user.repository.ProviderRepository;
 import br.com.jamadeu.gobarber.modules.user.repository.UserRepository;
+import br.com.jamadeu.gobarber.shared.exception.BadRequestException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -15,6 +18,7 @@ import org.springframework.stereotype.Service;
 public class AppointmentService {
     private final AppointmentRepository appointmentRepository;
     private final UserRepository userRepository;
+    private final ProviderRepository providerRepository;
 
     public Appointment findByIdOrThrowBadRequestException(long id) {
         return appointmentRepository.findById(id).orElseThrow(
@@ -22,25 +26,23 @@ public class AppointmentService {
         );
     }
 
-    public Page<Appointment> findByUserId(Long userId) {
-        if (userRepository.findById(userId).isEmpty()) {
-            throw new BadRequestException("User not found");
-        }
-        return appointmentRepository.findByUserId(userId, PageRequest.of(0, 5));
+    public Page<Appointment> findByUserId(Long id) {
+        GoBarberUser user = userRepository.findById(id)
+                .orElseThrow(() -> new BadRequestException("User not found"));
+        return appointmentRepository.findByUser(user, PageRequest.of(0, 5));
     }
 
-    public Page<Appointment> findByProviderId(Long providerId) {
-        if (userRepository.findById(providerId).isEmpty()) {
-            throw new BadRequestException("Provider not found");
-        }
-        return appointmentRepository.findByUserId(providerId, PageRequest.of(0, 5));
+    public Page<Appointment> findByProviderId(Long id) {
+        GoBarberProvider provider = providerRepository.findById(id)
+                .orElseThrow(() -> new BadRequestException("Provider not found"));
+        return appointmentRepository.findByProvider(provider, PageRequest.of(0, 5));
     }
 
     public Appointment create(NewAppointmentRequest newAppointmentRequest) {
-        if (userRepository.findById(newAppointmentRequest.getUserId()).isEmpty()) {
+        if (userRepository.findById(newAppointmentRequest.getUser().getId()).isEmpty()) {
             throw new BadRequestException("User not found");
         }
-        if (userRepository.findById(newAppointmentRequest.getProviderId()).isEmpty()) {
+        if (providerRepository.findById(newAppointmentRequest.getProvider().getId()).isEmpty()) {
             throw new BadRequestException("Provider not found");
         }
         return appointmentRepository.save(newAppointmentRequest.toAppointment());
