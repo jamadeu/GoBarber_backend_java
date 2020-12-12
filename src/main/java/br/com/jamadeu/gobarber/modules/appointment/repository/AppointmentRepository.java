b208e6d5610c68fa106dd88195aed81b2f001c27
Jean Amadeu <jean.amadeu2@gmail.com>
2020-12-12 12:43:10 -0300

    refact: refactor AppointmentRepository

--- src/main/java/br/com/jamadeu/gobarber/modules/appointment/repository/AppointmentRepository.java
+++ src/main/java/br/com/jamadeu/gobarber/modules/appointment/repository/AppointmentRepository.java
@@ -1,19 +1,16 @@
 package br.com.jamadeu.gobarber.modules.appointment.repository;
 
 import br.com.jamadeu.gobarber.modules.appointment.domain.Appointment;
+import br.com.jamadeu.gobarber.modules.user.domain.GoBarberProvider;
+import br.com.jamadeu.gobarber.modules.user.domain.GoBarberUser;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Date;
-import java.util.Optional;
-
 @Repository
 public interface AppointmentRepository extends JpaRepository<Appointment, Long> {
-    Page<Appointment> findByUserId(Long id, Pageable pageable);
+    Page<Appointment> findByUser(GoBarberUser user, Pageable pageable);
 
-    Page<Appointment> findByProviderId(Long id, Pageable pageable);
-
-    Optional<Appointment> findByProviderIdAndDate(Long providerId, Date date);
+    Page<Appointment> findByProvider(GoBarberProvider provider, Pageable pageable);
 }
