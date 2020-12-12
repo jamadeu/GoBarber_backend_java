099424859ffe88f7c7e7d1f18544f50e12a4420a
Jean Amadeu <jean.amadeu2@gmail.com>
2020-12-12 13:47:16 -0300

    test: AppointmentRepositoryTest first test passed

--- src/test/java/br/com/jamadeu/gobarber/modules/appointment/repository/AppointmentRepositoryTest.java
+++ src/test/java/br/com/jamadeu/gobarber/modules/appointment/repository/AppointmentRepositoryTest.java
@@ -1,8 +1,13 @@
 package br.com.jamadeu.gobarber.modules.appointment.repository;
 
 import br.com.jamadeu.gobarber.modules.appointment.domain.Appointment;
-import br.com.jamadeu.gobarber.util.AppointmentCreator;
+import br.com.jamadeu.gobarber.modules.user.domain.GoBarberProvider;
+import br.com.jamadeu.gobarber.modules.user.domain.GoBarberUser;
+import br.com.jamadeu.gobarber.modules.user.repository.ProviderRepository;
+import br.com.jamadeu.gobarber.modules.user.repository.UserRepository;
+import br.com.jamadeu.gobarber.util.UserCreator;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,18 +20,34 @@ import java.time.LocalDateTime;
 class AppointmentRepositoryTest {
     @Autowired
     private AppointmentRepository appointmentRepository;
+    @Autowired
+    private UserRepository userRepository;
+    private GoBarberUser user;
+    @Autowired
+    private ProviderRepository providerRepository;
+    private GoBarberProvider provider;
+
+    @BeforeEach
+    void setup() {
+        user = userRepository.save(UserCreator.createUserToBeSavedWithPasswordEncoded());
+        provider = providerRepository.save(UserCreator.createProviderToBeSavedWithPasswordEncoded());
+    }
 
     @Test
     @DisplayName("save persists appointment when successful")
     void save_PersistsAppointment_WhenSuccessful() {
-        Appointment appointmentToBeSaved = AppointmentCreator.createAppointmentToBeSaved();
+        Appointment appointmentToBeSaved = Appointment.builder()
+                .user(user)
+                .provider(provider)
+                .date(LocalDateTime.now())
+                .build();
         Appointment appointmentSaved = appointmentRepository.save(appointmentToBeSaved);
 
         Assertions.assertThat(appointmentSaved).isNotNull();
         Assertions.assertThat(appointmentSaved.getId()).isNotNull();
         Assertions.assertThat(appointmentSaved.getUser()).isEqualTo(appointmentToBeSaved.getUser());
         Assertions.assertThat(appointmentSaved.getProvider()).isEqualTo(appointmentToBeSaved.getProvider());
-        Assertions.assertThat(appointmentSaved.getDate()).isAfterOrEqualTo(LocalDateTime.now());
+        Assertions.assertThat(appointmentSaved.getDate()).isBefore(LocalDateTime.now());
     }
 
 }
