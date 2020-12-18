fa098fe5f274500735bfc455def7b3ae5f35db66
Jean Amadeu <jean.amadeu2@gmail.com>
2020-12-18 11:01:42 -0300

    chore: ProviderService

--- src/main/java/br/com/jamadeu/gobarber/modules/user/service/ProviderService.java
+++ src/main/java/br/com/jamadeu/gobarber/modules/user/service/ProviderService.java
@@ -29,20 +29,14 @@ public class ProviderService implements UserDetailsService {
     }
 
     public GoBarberProvider findByIdOrThrowBadRequestException(long id) {
-        return providerRepository.findById(id).orElseThrow(
-                () -> new BadRequestException("Provider not found")
-        );
+        return checkIfProviderExists(id);
     }
 
     @Transactional
     public GoBarberProvider save(NewProviderRequest newProviderRequest) {
         GoBarberProvider newProvider = UserMapper.INSTANCE.toProvider(newProviderRequest);
-        if (providerRepository.findByEmail(newProvider.getEmail()).isPresent()) {
-            throw new BadRequestException("This email is already in use: " + newProvider.getEmail());
-        }
-        if (providerRepository.findByUsername(newProvider.getUsername()).isPresent()) {
-            throw new BadRequestException("This username is already in use: " + newProvider.getUsername());
-        }
+        checkIfEmailAlreadyInUse(newProvider.getEmail());
+        checkIfUsernameAlreadyInUse(newProvider.getUsername());
         newProvider.setAuthorities("ROLE_PROVIDER");
         String passwordEncoded = passwordEncoder.encode(newProvider.getPassword());
         newProvider.setPassword(passwordEncoded);
@@ -52,15 +46,12 @@ public class ProviderService implements UserDetailsService {
     @Transactional
     public void replace(ReplaceProviderRequest replaceProviderRequest) {
         GoBarberProvider providerToReplace = UserMapper.INSTANCE.toProvider(replaceProviderRequest);
-        GoBarberProvider savedProvider = providerRepository.findById(providerToReplace.getId())
-                .orElseThrow(() -> new BadRequestException("Provider not found"));
-        if (!providerToReplace.getEmail().equals(savedProvider.getEmail()) &&
-                providerRepository.findByEmail(providerToReplace.getEmail()).isPresent()) {
-            throw new BadRequestException("This email is already in use: " + providerToReplace.getEmail());
+        GoBarberProvider savedProvider = checkIfProviderExists(providerToReplace.getId());
+        if (!providerToReplace.getEmail().equals(savedProvider.getEmail())) {
+            checkIfEmailAlreadyInUse(providerToReplace.getEmail());
         }
-        if (!providerToReplace.getUsername().equals(savedProvider.getUsername()) &&
-                providerRepository.findByUsername(providerToReplace.getUsername()).isPresent()) {
-            throw new BadRequestException("This username is already in use: " + providerToReplace.getUsername());
+        if (!providerToReplace.getUsername().equals(savedProvider.getUsername())) {
+            checkIfUsernameAlreadyInUse(providerToReplace.getUsername());
         }
         providerToReplace.setPassword(savedProvider.getPassword());
         providerRepository.save(providerToReplace);
@@ -73,17 +64,37 @@ public class ProviderService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) {
-        return providerRepository.findByUsername(username)
-                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+        return checkIfProviderExists(username);
     }
 
     public void resetPassword(ResetPasswordRequest resetPasswordRequest) {
-        GoBarberProvider provider = providerRepository.findByUsername(resetPasswordRequest.getUsername())
-                .orElseThrow(() -> new BadRequestException("Provider not found"));
+        GoBarberProvider provider = checkIfProviderExists(resetPasswordRequest.getUsername());
         if (!passwordEncoder.matches(resetPasswordRequest.getOldPassword(), provider.getPassword())) {
             throw new BadRequestException("Old password is wrong");
         }
         provider.setPassword(passwordEncoder.encode(resetPasswordRequest.getNewPassword()));
         providerRepository.save(provider);
+    }
+
+    private GoBarberProvider checkIfProviderExists(Long userId) {
+        return providerRepository.findById(userId)
+                .orElseThrow(() -> new BadRequestException("Provider not found"));
+    }
+
+    private GoBarberProvider checkIfProviderExists(String username) {
+        return providerRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Provider not found"));
+    }
+
+    private void checkIfEmailAlreadyInUse(String email) {
+        if (providerRepository.findByEmail(email).isPresent()) {
+            throw new BadRequestException("This email is already in use: " + email);
+        }
+    }
+
+    private void checkIfUsernameAlreadyInUse(String username) {
+        if (providerRepository.findByUsername(username).isPresent()) {
+            throw new BadRequestException("This username is already in use: " + username);
+        }
     }
 }
