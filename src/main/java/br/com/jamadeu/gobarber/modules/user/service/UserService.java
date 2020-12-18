43232a934a9981a3511b24e8df2b14635def87ff
Jean Amadeu <jean.amadeu2@gmail.com>
2020-12-18 10:51:43 -0300

    chore: UserService

--- src/main/java/br/com/jamadeu/gobarber/modules/user/service/UserService.java
+++ src/main/java/br/com/jamadeu/gobarber/modules/user/service/UserService.java
@@ -29,20 +29,14 @@ public class UserService implements UserDetailsService {
     }
 
     public GoBarberUser findByIdOrThrowBadRequestException(long id) {
-        return userRepository.findById(id).orElseThrow(
-                () -> new BadRequestException("User not found")
-        );
+        return checkIfUserExists(id);
     }
 
     @Transactional
     public GoBarberUser save(NewUserRequest newUserRequest) {
         GoBarberUser newUser = UserMapper.INSTANCE.toUser(newUserRequest);
-        if (userRepository.findByEmail(newUser.getEmail()).isPresent()) {
-            throw new BadRequestException("This email is already in use: " + newUser.getEmail());
-        }
-        if (userRepository.findByUsername(newUser.getUsername()).isPresent()) {
-            throw new BadRequestException("This username is already in use: " + newUser.getUsername());
-        }
+        checkIfEmailAlreadyInUse(newUser.getEmail());
+        checkIfUsernameAlreadyInUse(newUser.getUsername());
         newUser.setAuthorities("ROLE_USER");
         String passwordEncoded = passwordEncoder.encode(newUser.getPassword());
         newUser.setPassword(passwordEncoded);
@@ -52,15 +46,12 @@ public class UserService implements UserDetailsService {
     @Transactional
     public void replace(ReplaceUserRequest replaceUserRequest) {
         GoBarberUser userToReplace = UserMapper.INSTANCE.toUser(replaceUserRequest);
-        GoBarberUser savedUser = userRepository.findById(userToReplace.getId())
-                .orElseThrow(() -> new BadRequestException("User not found"));
-        if (!userToReplace.getEmail().equals(savedUser.getEmail()) &&
-                userRepository.findByEmail(userToReplace.getEmail()).isPresent()) {
-            throw new BadRequestException("This email is already in use: " + userToReplace.getEmail());
+        GoBarberUser savedUser = checkIfUserExists(userToReplace.getId());
+        if (!userToReplace.getEmail().equals(savedUser.getEmail())) {
+            checkIfEmailAlreadyInUse(userToReplace.getEmail());
         }
-        if (!userToReplace.getUsername().equals(savedUser.getUsername()) &&
-                userRepository.findByUsername(userToReplace.getUsername()).isPresent()) {
-            throw new BadRequestException("This username is already in use: " + userToReplace.getUsername());
+        if (!userToReplace.getUsername().equals(savedUser.getUsername())) {
+            checkIfUsernameAlreadyInUse(userToReplace.getUsername());
         }
         userToReplace.setPassword(savedUser.getPassword());
         userRepository.save(userToReplace);
@@ -73,17 +64,37 @@ public class UserService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) {
-        return userRepository.findByUsername(username)
-                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+        return checkIfUserExists(username);
     }
 
     public void resetPassword(ResetPasswordRequest resetPasswordRequest) {
-        GoBarberUser user = userRepository.findByUsername(resetPasswordRequest.getUsername())
-                .orElseThrow(() -> new BadRequestException("User not found"));
+        GoBarberUser user = checkIfUserExists(resetPasswordRequest.getUsername());
         if (!passwordEncoder.matches(resetPasswordRequest.getOldPassword(), user.getPassword())) {
             throw new BadRequestException("Old password is wrong");
         }
         user.setPassword(passwordEncoder.encode(resetPasswordRequest.getNewPassword()));
         userRepository.save(user);
+    }
+
+    private GoBarberUser checkIfUserExists(Long userId) {
+        return userRepository.findById(userId)
+                .orElseThrow(() -> new BadRequestException("User not found"));
+    }
+
+    private GoBarberUser checkIfUserExists(String username) {
+        return userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    }
+
+    private void checkIfEmailAlreadyInUse(String email) {
+        if (userRepository.findByEmail(email).isPresent()) {
+            throw new BadRequestException("This email is already in use: " + email);
+        }
+    }
+
+    private void checkIfUsernameAlreadyInUse(String username) {
+        if (userRepository.findByUsername(username).isPresent()) {
+            throw new BadRequestException("This username is already in use: " + username);
+        }
     }
 }
